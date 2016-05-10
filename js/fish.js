55c9f523558a987bba6afd3e7e9df47f411faba7
Jordan Lin <jordan.lin09@gmail.com>
2016-05-09 18:11:56 -0700

    reset

--- js/fish.js
+++
@@ -1,9 +0,0 @@
-var Fish = function (x, y, panelInfo) {
-    this.x = x;
-    this.y = y;
-    this.pinfo = panelInfo; //the information window of the game
-    this.life = 10; //blood
-    this.init();  //initialize
-};
-
-Fish.prototype
