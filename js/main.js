9b901f77bc37828678d1e15fb14322aa0dee8308
Lei Pei <hileopei@gmail.com>
2016-05-12 18:42:12 -0700

    updated game to current V0.4

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ function init() {
 	document.getElementById("js_start_loading").style.display = "none";
 	document.getElementById("js_start_btn").style.display = "block";
 	document.getElementById("js_tutor_btn").style.display = "block";
-
+	
 }
 
 function start() {
@@ -93,7 +93,7 @@ function finishClick() {
             //alert("stupid man");
             win = false;
             over(win);
-        }
+        }  
     }
 }
 
@@ -135,8 +135,8 @@ function updateLevel() {
 }
 
 function over(flag){
-
-
+		
+  
     //this.timeQuene.stop();
 
 
@@ -184,7 +184,7 @@ $(document).keydown(function(event) {
             //document.getElementById('imgfish').src = './img/crab.png';   //for testing
             clearInterval(movingTimer);
             movingTimer = null;
-            movingTimer = setInterval(' moveLeft();', 10);
+            movingTimer = setInterval(' moveLeft();', 10);         
             break;
         case 38: // up
             //change picture here
