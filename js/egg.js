e69395e0131d05c9140eff36d9c39c54a6eec4b9
Lei Pei <hileopei@gmail.com>
2016-05-13 08:33:28 -0700

    easter egg updated

--- js/egg.js
+++ js/egg.js
@@ -1,14 +1,40 @@
 var eggCounter = 0;
+var h = 180;
+var w = 200;
 
 function clickEgg() {
   if(eggCounter == 5) {
     serveSashimi();
   } else {
-    eggCounter += 1;
+      eggCounter += 1;
+      changePic();
   }
 }
 
 function serveSashimi() {
-  var blowfish = document.getElementById("myBlowfish");
-  blowfish.src = "./img/Sashimi.png";
+    var blowfish = document.getElementById('myblowfish');
+    blowfish.src = "./img/Sashimi.png";
+    $("#myblowfish").animate({
+        //left: '250px',
+        //opacity: '0.5',
+        height: '280px',
+        width: '320px'
+    }, 2000);
+}
+
+function changePic() {
+    var blowfishh = document.getElementById('myblowfish');
+    //blowfish.src = "./img/Sashimi.png";
+    var img = "./img/" + parseInt(eggCounter) + ".gif";
+    blowfishh.src = img;
+    h += 15;
+    w += 15;
+
+    $("#myblowfish").animate({
+        //left: '250px',
+        //opacity: '0.5',
+        height: h + 'px',
+        width:  w + 'px'
+    }, 1000);
+   
 }
